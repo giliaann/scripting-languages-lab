9679ad31433bda3aa5dbd38157c08059bbd48257
dubajstan <dubajstan@gmail.com>
2026-05-07 02:07:09 +0200

    changed to return absolute path

--- lab5/group_measurement_files_by_key.py
+++ lab5/group_measurement_files_by_key.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import re
+from pprint import pprint
 
 '''
 This function extracts measurements files (<year>_<measurand>_<frequency>.csv) from the given directory
@@ -20,9 +21,9 @@ def group_measurement_files_by_key(path: Path):
             
         pattern = re.compile(r"^(\d{4})_([^_]+)_([^.]+)\.csv$")
         if (match := pattern.match(file_path.name)):
-            result[match.groups()] = file_path
+            result[match.groups()] = file_path.absolute()
 
     return result
 
 if __name__ == "__main__":
-    print(group_measurement_files_by_key(Path('lab5/data/measurements')))
+    pprint(group_measurement_files_by_key(Path('lab5/data/measurements')))
