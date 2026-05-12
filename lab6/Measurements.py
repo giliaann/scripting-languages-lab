3520521de40553f6329a65c5120e9c6f7d2c481b
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-05-12 17:23:43 +0200

    add task 5

---
+++ lab6/Measurements.py
@@ -0,0 +1,120 @@
+import csv
+import re
+from pathlib import Path
+from typing import List, Dict, Set, Union
+from TimeSeries import TimeSeries
+from utils import parse_measurement_file
+
+class Measurements:
+    def __init__(self, directory_path: Union[str, Path]):
+        
+        self.directory_path = Path(directory_path)
+        
+        self._files_metadata: Dict[Path, Dict] = {}
+        self._station_to_files: Dict[str, Set[Path]] = {}
+        self._file_to_ts_count: Dict[Path, int] = {}
+        self._cache: Dict[Path, List[TimeSeries]] = {}
+
+        self._identify_files_by_name()
+
+    def _identify_files_by_name(self):
+        if not self.directory_path.is_dir():
+            raise FileNotFoundError(f"{self.directory_path} is not a directory")
+
+        pattern = re.compile(r"(\d{4})_(.*?)_(.*?)\.csv")
+        
+        for file_path in self.directory_path.iterdir():
+            if file_path.is_file() and file_path.suffix == '.csv':
+                match = pattern.match(file_path.name)
+                if match:
+                    self._files_metadata[file_path] = {
+                        'year': match.group(1),
+                        'parameter': match.group(2),
+                        'freq': match.group(3)
+                    }
+
+    def _build_station_registry(self):
+        """Reads only first two lines in all files. Checks which stations are included and count them for each file"""
+        if self._station_to_files and self._file_to_ts_count:
+            return
+            
+        for path in self._files_metadata:
+
+            with path.open(mode='r', encoding='utf-8-sig') as f:
+                
+                reader = csv.reader(f)
+                try:
+                    #skip ids
+                    next(reader)
+
+                    stations_codes_line = next(reader)[1:]
+                    stations_codes = [s.strip() for s in stations_codes_line if s.strip()]
+                    
+                    self._file_to_ts_count[path] = len(stations_codes)
+                    for s in stations_codes:
+                        if s not in self._station_to_files:
+                            self._station_to_files[s] = set()
+                        self._station_to_files[s].add(path)
+
+                except StopIteration:
+                    continue
+                    
+        self._is_indexed = True
+
+    def _get_ts(self, path: Path) -> List[TimeSeries]:
+        '''Reads data from TimeSeries cache or reads it into cache and returns it.'''
+        if path in self._cache:
+            return self._cache[path]
+
+        series_in_file = parse_measurement_file(path)
+        
+        self._cache[path] = series_in_file
+        return series_in_file
+
+    def __len__(self) -> int:
+        '''Returns number of TimeSeries from all measurements files'''
+        self._build_station_registry()
+        return sum(self._file_to_ts_count.values())
+
+    def __contains__(self, param_name: str) -> bool:
+        '''Checks if any station measures given measurement'''
+        return any(meta['parameter'] == param_name 
+                   for meta in self._files_metadata.values())
+
+    def get_by_parameter(self, param_name: str) -> List[TimeSeries]:
+        '''Returns list of all TimeSeries refering to given measurand'''
+        results = []
+        for path, meta in self._files_metadata.items():
+            if meta['parameter'] == param_name:
+                results.extend(self._get_ts(path))
+        return results
+
+    def get_by_station(self, station_code: str) -> List[TimeSeries]:
+        '''Returns list of all TimeSeries measured by a station with given code'''
+        self._build_station_registry()
+        results = []
+        if station_code in self._station_to_files:
+            for path in self._station_to_files[station_code]:
+                all_ts = self._get_ts(path)
+                results.extend([ts for ts in all_ts if ts.station_code == station_code])
+        return results
+    
+if __name__ == "__main__":
+    base_path = Path(__file__).parent
+    measurements_folder = base_path / "data" / "measurements"
+    ms = Measurements(measurements_folder)
+    
+    assert('CO' in ms)
+    assert('PYTHON' not in ms)
+
+    measurand = 'CO'
+    print(f'TimeSeries for {measurand} measurand')
+    print([f'| {x.indicator_name}, {x.station_code}, {x.mean:.2} {x.unit} |' for x in ms.get_by_parameter(measurand)[:5]])
+    
+    code = 'DsJelGorOgin'
+    print(f'TimeSeries for station "{code}"')
+    print([f'| {x.indicator_name}, {x.station_code}, {x.mean:.2} {x.unit} |' for x in ms.get_by_station(code)[:5]])
+    
+
+
+    
---
+++ lab6/utils.py
@@ -0,0 +1,74 @@
+import csv
+import numpy as np
+from datetime import datetime
+from pathlib import Path
+from typing import List, Union
+from TimeSeries import TimeSeries
+
+
+def parse_measurement_file(file_path: Union[str, Path]) -> List[TimeSeries]:
+    """
+    Parses measurment file. Retuns list of TimeSeries.
+    """
+    
+    path = Path(file_path)
+
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {path}")
+
+    with path.open(mode='r', encoding='utf-8-sig') as csvfile:
+        reader = list(csv.reader(csvfile, delimiter=','))
+        
+        if len(reader) < 7:
+            raise ValueError(f"File {path.name} is invalid - not enough lines.")
+
+        station_codes = [s.strip() for s in reader[1][1:] if s.strip()]
+        indicators = reader[2][1:]
+        averaging_times = reader[3][1:]
+        units = reader[4][1:]
+        
+        num_stations = len(station_codes)
+        
+        dates = []
+        all_values = [[] for _ in range(num_stations)]
+
+        
+        for row in reader[6:]:
+            if not row or not row[0].strip(): 
+                continue
+            
+            try:
+               
+                date_obj = datetime.strptime(row[0].strip(), '%d/%m/%y %H:%M')
+                dates.append(date_obj)
+
+            except ValueError:
+                continue
+
+            for i in range(num_stations):
+               
+                if i + 1 < len(row):
+                    val_str = row[i + 1].strip().replace(',', '.')
+                    if val_str == '':
+                        all_values[i].append(None)
+                    else:
+                        try:
+                            all_values[i].append(float(val_str))
+                        except ValueError:
+                            all_values[i].append(None)
+                else:
+                    all_values[i].append(None)
+
+        results = []
+        for i in range(num_stations):
+            ts = TimeSeries(
+                indicator_name=indicators[i].strip(),
+                station_code=station_codes[i],
+                averaging_time=averaging_times[i].strip(),
+                dates=dates,
+                values=np.array(all_values[i], dtype=object),
+                unit=units[i].strip()
+            )
+            results.append(ts)
+            
+        return results
