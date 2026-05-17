5b6d319494846f8895cda47868269545e6b8fdd4
Przemyslaw Mazurowski <przemyslaw.mazurowski@gmail.com>
2026-05-17 20:13:27 +0200

    add task1 def

--- lab7/task1.py
+++ lab7/task1.py
@@ -1,4 +1,4 @@
-from functools import reduce, partial
+from functools import reduce
 import operator
 
 def acronym(xs: list[str]) -> str:
@@ -23,3 +23,32 @@ def root(x: int | float, epislon: int | float) -> int | float:
     return root_in(1)
 
 print(root(3, 0.1))
+
+def make_alpha_dict(text: str) -> dict[str, list[str]]:
+    words = text.split()
+    return {
+        char: [word for word in words if char in word]
+        for char in dict.fromkeys(text) if char.isalpha()
+    }
+
+print(make_alpha_dict("on i ona"))
+
+def extend_and_return(x, y):
+    x.extend(y)
+    return x
+
+def flatten(xs: list) -> list:
+    return reduce(extend_and_return ,([x] if not isinstance(x, (list, tuple)) else flatten(x) for x in xs), [])
+
+print(flatten([[[],1], [2, 3], [[4, 5], 6]]))
+
+def group_anagrams(words: list[str]) -> dict[str, list[str]]:
+    
+    keys = dict.fromkeys("".join(sorted(word)) for word in words)
+    
+    return {
+        key: [word for word in words if "".join(sorted(word)) == key]
+        for key in keys
+    }
+
+print(group_anagrams(['kot', 'tok', 'pies', 'kep','pek']))
