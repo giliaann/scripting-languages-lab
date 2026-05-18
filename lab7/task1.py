c4dcbda14e97c12f683d59859897e2a1455c448f
dubajstan <dubajstan@gmail.com>
2026-05-18 14:39:33 +0200

    fixed task 1

--- lab7/task1.py
+++ lab7/task1.py
@@ -5,7 +5,7 @@ def acronym(xs: list[str]) -> str:
     return reduce(operator.add, map(lambda xs: xs[0], xs))
 
 print(acronym(['Wielkie', 'Zagłębie', 'Lubin']))
-
+ 
 
 def median(xs: list[int | float]) -> int | float:
     length = len(xs)
@@ -15,14 +15,13 @@ def median(xs: list[int | float]) -> int | float:
 
 print(median([1,1,19,2,3,4,4,5,1]))
 
-def root(x: int | float, epislon: int | float) -> int | float:
-    
-    def root_in(y: int | float) -> int | float:
-        return y if abs(y**2 - x) < epislon else root_in(0.5*(y + x/y))
-        
-    return root_in(1)
+def pierwiastek(x: float, epsilon: float) -> float:
+    def aux(y: float) -> float:
+        return y if abs(y*y - x) < epsilon else aux(0.5 * (y + x/y))
+    return aux(x) if x != 0.0 else 0.0
 
-print(root(3, 0.1))
+print(pierwiastek(5.0, 0.01))
+
 
 def make_alpha_dict(text: str) -> dict[str, list[str]]:
     words = text.split()
@@ -33,22 +32,24 @@ def make_alpha_dict(text: str) -> dict[str, list[str]]:
 
 print(make_alpha_dict("on i ona"))
 
-def extend_and_return(x, y):
-    x.extend(y)
-    return x
-
-def flatten(xs: list) -> list:
-    return reduce(extend_and_return ,([x] if not isinstance(x, (list, tuple)) else flatten(x) for x in xs), [])
+def flatten(data: list) -> list:
+    match data:
+        case [] | ():
+            return []
+        case [h, *t] if not isinstance(data, (str, bytes)):
+            return flatten(h) + flatten(t)
+        case _:
+            return [data]
 
 print(flatten([[[],1], [2, 3], [[4, 5], 6]]))
 
 def group_anagrams(words: list[str]) -> dict[str, list[str]]:
-    
     keys = dict.fromkeys("".join(sorted(word)) for word in words)
     
     return {
         key: [word for word in words if "".join(sorted(word)) == key]
         for key in keys
     }
+
 
 print(group_anagrams(['kot', 'tok', 'pies', 'kep','pek']))
