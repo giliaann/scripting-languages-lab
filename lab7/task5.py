51f5252bb360410b90039ea09533f87a835c8c45
dubajstan <dubajstan@gmail.com>
2026-05-19 12:54:12 +0200

    modified task5

--- lab7/task5.py
+++ lab7/task5.py
@@ -1,14 +1,23 @@
 import functools
 import sys
 from task4 import make_generator, catalan_nth, fibonacci_nth
-
+import types
 
 
 
 def make_generator_mem(f):
-    cached = functools.lru_cache(maxsize=None)(f)
-    module = sys.modules[f.__module__]
-    setattr(module, f.__name__, cached)
+    new_globals = f.__globals__.copy()
+    function_copy = types.FunctionType(
+        f.__code__,
+        new_globals,
+        name=f.__name__,
+        argdefs=f.__defaults__,
+        closure=f.__closure__
+    )
+    
+    cached = functools.lru_cache(maxsize=None)(function_copy)
+    new_globals[f.__name__] = cached
+    
     return make_generator(cached)
 
 
@@ -16,6 +25,15 @@ def make_generator_mem(f):
     
 
 if __name__ == '__main__':
-    gen = make_generator_mem(fibonacci_nth)
-    for _ in range(40):
+    gen_mem = make_generator_mem(fibonacci_nth)
+    for _ in range(38):
+        print(next(gen_mem))
+    
+    gen = make_generator(fibonacci_nth)
+    for _ in range(38):
         print(next(gen))
+
+    
+
+    #module = sys.modules[f.__module__]
+    #setattr(module, f.__name__, cached)
